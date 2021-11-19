4d27c0780be4441d0189f57f2ae33d941e7f41f2
Mosaddek Sohel <tanjimsohel>
2021-11-19 22:04:52 +0600

    new add

--- index.js
+++ index.js
@@ -10,13 +10,17 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.10dvn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.10dvn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const uri = `mongodb+srv://${process.env.DB_USER}::${process.env.DB_PASS}@cluster0.10dvn.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
 
 async function run() {
     try {
         await client.connect();
-        const database = client.db('bikeMountain');
+        const database = client.db('bdTurism');
         const productsCollection = database.collection('products');
         const userCollection = database.collection('users')
         const userCollectionRatting = database.collection('ratting')
